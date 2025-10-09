d94fb2d5d34a948fb203a9d7db193019a11751e5
LeonardoVieiraGuimaraes <leonardovieiraxy@hotmail.com>
2025-10-08 22:10:18 -0300

    Changes to be committed: 	modified:   MiniCursoPalestra/CNN/inference_colab.ipynb 	modified:   redeNeuralArtificial/DodCat/cats_and_dogs_filtered/vectorize.py

--- redeNeuralArtificial/DodCat/cats_and_dogs_filtered/vectorize.py
+++ redeNeuralArtificial/DodCat/cats_and_dogs_filtered/vectorize.py
@@ -2,5 +2,20 @@ from keras.preprocessing.image import array_to_img, img_to_array, load_img
 
 import os
 
-for fname in os.listdir('train/cats'):
-  print fname
+
+def list_cat_images(train_dir='train/cats'):
+  """Lista arquivos de imagem no diretório de treino de gatos.
+
+  Retorna uma lista de nomes de arquivos. Emite uma mensagem se o
+  diretório não existir.
+  """
+  if not os.path.isdir(train_dir):
+    print(f"Diretório não encontrado: {train_dir}")
+    return []
+  return [fname for fname in os.listdir(train_dir) if os.path.isfile(os.path.join(train_dir, fname))]
+
+
+if __name__ == '__main__':
+  images = list_cat_images()
+  for fname in images:
+    print(fname)
